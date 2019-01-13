d58734f226a72cc9788163fe6c6bfdc9c5c496aa
billyzou <yz3352@nyu.edu>
2019-01-13 16:44:35 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <span class="my-icon">
+            <font-awesome-icon :icon="['fas', 'user']"></font-awesome-icon>
+        </span>
+        <font-awesome-icon :icon="['fab', 'weixin']"></font-awesome-icon>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+    };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .my-icon {
+        color: red;
+        background-color: blue;
+        font-size: 32px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+    faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+    faWeixin,
+} from '@fortawesome/free-brands-svg-icons';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+library.add(
+    faUser,
+    faWeixin,
+);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+    render: function (h) {
+        return h(App);
+    },
+}).$mount('#app');
